14a409f488cbc4eee29f970e7812ec4fe0eaef44
mingtianyihou33 <liumingyi1428@126.com>
2020-01-15 15:03:44 +0800

    feat: add more emoji

--- src/index.js
+++ src/index.js
@@ -8,11 +8,16 @@ function splice(str, index, count, add) {
   return str.slice(0, index) + add + str.slice(index + count);
 }
 
+let labelEmotion = `<a title="{{title}}" style="display: inline-block;background: url(https://res.wx.qq.com/a/wx_fed/webwx/res/static/img/6AfH8-r.png) no-repeat;width: 28px;
+    height: 28px; background-position:{{position}};"></a>`;
+
 export default function emojiParser(str) {
   let matchedEmoji = trie.search(str);
   matchedEmoji.map((idx) => {
-    let pos = idx[0], emotion = emojiKeys[idx[1]],
-      img = '<img src="' + emojiObj[emotion] + '" alt="' + emotion + '">';
+    let pos = idx[0], emotion = emojiKeys[idx[1]];
+    let img = /^:.*:$|^\[.*\]$/.test(emotion)
+      ? labelEmotion.replace('{{title}}', emotion).replace('{{position}}', emojiObj[emotion])
+      : '<img src="' + emojiObj[emotion] + '" alt="' + emotion + '">'
     str = splice(str, pos, emotion.length, img);
   });
   return str;
