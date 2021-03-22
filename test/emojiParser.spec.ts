2d04d0022709bcea2fd8e15751413df5d0ecbd17
mingtianyihou33 <liumingyi1428@126.com>
2021-03-22 11:27:40 +0800

    test: add test item

--- test/emojiParser.spec.ts
+++ test/emojiParser.spec.ts
@@ -13,4 +13,9 @@ describe('parseEmoji', function () {
       '<a title="[右哼哼]" class="wx-emoji" style="display: inline-block;background: url(http://xxx.png) no-repeat;width: 64px;height: 64px;background-position: 0px 0px;background-size: 767.61px;"></a>',
     )
   })
+  test('input sdfasdfa should return sdfasdfa', () => {
+    expect(parseEmoji('sdfasdfa')).toBe(
+      'sdfasdfa',
+    )
+  })
 })
